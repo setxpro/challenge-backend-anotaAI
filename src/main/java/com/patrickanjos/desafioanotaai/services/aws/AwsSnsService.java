4857004b0b69c57626fd737ec3d8f1041d1abb95
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-01-18 09:35:26 -0300

    feat(service): create AwsSnsService

---
+++ src/main/java/com/patrickanjos/desafioanotaai/services/aws/AwsSnsService.java
@@ -0,0 +1,22 @@
+package com.patrickanjos.desafioanotaai.services.aws;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AwsSnsService {
+    AmazonSNS snsClient;
+
+    Topic catalogTopic;
+
+    public AwsSnsService(AmazonSNS snsClient, @Qualifier("catalogEventsTopic") Topic catalogTopic) {
+        this.snsClient = snsClient;
+        this.catalogTopic = catalogTopic;
+    }
+
+    public void publish(MessageDTO message) {
+         this.snsClient.publish(catalogTopic.getTopicArn(), message.toString());
+    }
+}
---
+++ src/main/java/com/patrickanjos/desafioanotaai/services/aws/MessageDTO.java
@@ -0,0 +1,4 @@
+package com.patrickanjos.desafioanotaai.services.aws;
+
+public record MessageDTO(String message) {
+}
