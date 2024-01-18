ade7825a634def207e8b0b32e1ba767d98224659
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-01-18 06:54:21 -0300

    feat: config database

---
+++ src/main/java/com/patrickanjos/desafioanotaai/config/MongoDBConfig.java
@@ -0,0 +1,21 @@
+package com.patrickanjos.desafioanotaai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        // Connection String
+        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/product-catalog");
+    }
+
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDatabaseFactory());
+    }
+}
---
+++ src/test/java/com/patrickanjos/desafioanotaai/DesafioAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickanjos.desafioanotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
